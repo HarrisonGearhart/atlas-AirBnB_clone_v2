64cef1264d905097a0bd0ac716709e5dacc84feb
Harrison  Gearhart <harrisongearhart@Harrisons-Air.attlocal.net>
2024-11-05 13:08:44 -0600

    6-num

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -42,7 +42,7 @@ def numbersandtemplates(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def numbersandevenness(n):
     """display a html page only if n is an integer"""
     if n % 2 == 0:
